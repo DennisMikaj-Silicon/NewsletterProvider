a15398ac580facb31ab3e887777dcf19cb36c530
Dennis <mikajdennis@gmail.com>
2024-05-15 10:38:05 +0200

    Add project files.

---
+++ Data/Entities/SubscribeEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+public class SubscribeEntity
+{
+	[Required]
+	[Key]
+	public string Email { get; set; } = null!;
+
+	public bool IsSubscribed { get; set; } = false;
+	public bool DailyNewsletter { get; set; }
+	public bool AdvertisingUpdates { get; set; }
+	public bool WeekinReview { get; set; }
+	public bool EventUpdates { get; set; }
+	public bool StartupsWeekly { get; set; }
+	public bool Podcasts { get; set; }
+}
